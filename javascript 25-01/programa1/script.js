128c4567847203891a11bf45212d8aa135c48212
unknown <gabrielvital0917@digitalcollege.internal>
2025-02-01 12:00:43 -0300

    JS

---
+++ javascript 25-01/programa1/script.js
@@ -0,0 +1,10 @@
+// Exercicio 3:
+// Usaar o prompt para perguntar a larugra e a altura de um retângulo e retornar a área.
+// let largura = prompt("Dgite a largura do retângulo: ");
+// let altura = prompt("Digite a altura do retângulo: ");
+
+// const area = largura*altura;
+// const perimetro = Number(largura)*2 + Number(altura)*2;
+
+// console.log("A área do retângulo é igual a: " + area);
+// console.log("O perimetro é igual a: " + perimetro);
